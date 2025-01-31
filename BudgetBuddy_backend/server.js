f130dbdb5ac9062de6170d2a49d9c5608f7f445e
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2025-01-31 09:57:27 +0530

    env update

--- BudgetBuddy_backend/server.js
+++ BudgetBuddy_backend/server.js
@@ -1,37 +1,40 @@
 import express from 'express';
 import cors from 'cors';
 import dbconnect from './db/mongodbconnect.js';
-import {saveNewUser} from './controller/userController.js';
+import { saveNewUser } from './controller/userController.js';
 import { deleteBudget, fetchBudgetsByUserId, saveNewBudget, updateBudget } from './controller/budgetController.js';
 import { deleteExpense, fetchExpenses, fetchRecentExpenses, saveNewExpense, updateExpense } from './controller/expenseController.js';
 import { fetchDashboard } from './controller/dashboard.js';
 import { generateReport, getBudgetById } from './controller/Report.js';
 import { addReminder } from './controller/remaindercontroller.js';
 
-
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT || 3000; // Default to 3000 if PORT is not set
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017'; // Use environment variable
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
-dbconnect();
+// Connect to MongoDB
+dbconnect(mongoURI);
 
-app.post('/saveUser',saveNewUser);
-app.post('/addbudget',saveNewBudget);
-app.get('/getbudgets',fetchBudgetsByUserId);
-app.post('/addexpense',saveNewExpense);
-app.get('/getexpenses',fetchExpenses);
-app.get('/getlatestexpenses',fetchRecentExpenses);
-app.get('/getDashBoardData',fetchDashboard);
-app.get('/getexpensesinrange', generateReport); 
+// Define routes
+app.post('/saveUser', saveNewUser);
+app.post('/addbudget', saveNewBudget);
+app.get('/getbudgets', fetchBudgetsByUserId);
+app.post('/addexpense', saveNewExpense);
+app.get('/getexpenses', fetchExpenses);
+app.get('/getlatestexpenses', fetchRecentExpenses);
+app.get('/getDashBoardData', fetchDashboard);
+app.get('/getexpensesinrange', generateReport);
 app.get('/getbudgetdata', getBudgetById);
-app.put('/updatebudget/:budgetId',updateBudget);
-app.delete('/deletebudget/:budgetId',deleteBudget);
-app.put('/updateexpense/:expenseId',updateExpense);
-app.delete('/deleteexpense/:expenseId',deleteExpense);
-app.post('/remainders',addReminder);
+app.put('/updatebudget/:budgetId', updateBudget);
+app.delete('/deletebudget/:budgetId', deleteBudget);
+app.put('/updateexpense/:expenseId', updateExpense);
+app.delete('/deleteexpense/:expenseId', deleteExpense);
+app.post('/remainders', addReminder);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
